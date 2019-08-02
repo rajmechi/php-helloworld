d2bc3156f8abdb3f10e2f26e6a893377c9205f5b
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-08-01 21:18:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,21 @@
  </head>
  <body>
  <?php 
-   print_r($_REQUEST);
+   foreach ($_SERVER as $key => $value) {
+    if (strpos($key, 'HTTP_') === 0) {
+        $chunks = explode('_', $key);
+        $header = '';
+        for ($i = 1; $y = sizeof($chunks) - 1, $i < $y; $i++) {
+            $header .= ucfirst(strtolower($chunks[$i])).'-';
+        }
+        $header .= ucfirst(strtolower($chunks[$i])).': '.$value;
+        echo $header."\n";
+    }
+}
+$body = file_get_contents('php://input');
+if ($body != '') {
+  print("\n$body\n\n");
+}
 ?>
  </body>
 </html>
