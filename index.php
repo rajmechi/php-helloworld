079c293476a0fb453fc639bee18e0e7a72879627
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-08-01 21:04:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
  </head>
  <body>
  <?php 
-   print $_SERVER['HTTP_CLIENT_IP']
-   print $_SERVER['HTTP_X_FORWARDED_FOR']
-   print $_SERVER['REMOTE_ADDR']
+   print $_SERVER['HTTP_CLIENT_IP'];
+   print $_SERVER['HTTP_X_FORWARDED_FOR'];
+   print $_SERVER['REMOTE_ADDR'];
 ?>
  </body>
 </html>
