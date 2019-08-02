078eec51aef8d75d3dba3380fa029020aa76cf47
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-08-01 21:49:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,17 @@
         }
         $header .= ucfirst(strtolower($chunks[$i])).': '.$value;
         echo $header."\n";
-    }
-}
-$body = file_get_contents('php://input');
-if ($body != '') {
-  print("\n$body\n\n");
-}
+        }
+     }
+   $body = file_get_contents('php://input');
+   if ($body != '') {
+     #print("\n$body\n\n");
+   }
+   print "<br>HTTP_X_FORWARDED_FOR is " . $_SERVER['HTTP_X_FORWARDED_FOR'];
+   print "<br> HTTP_X_FORWARDED_HOST is " . $_SERVER['HTTP_X_FORWARDED_HOST'];
+   print "<br>HTTP_X_FORWARDED_SERVER is " . $_SERVER['HTTP_X_FORWARDED_SERVER'];
+   print "<br>SERVER_NAME is " . $_SERVER['SERVER_NAME'];
+
 ?>
  </body>
 </html>
